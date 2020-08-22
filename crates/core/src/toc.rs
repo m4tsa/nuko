d1e099d4010be2337517a89c7cfb72c937e4ac1b
Mats A <mats@mats.sh>
2020-08-22 12:21:01 +0200

    add num param to toc sections

--- crates/core/src/toc.rs
+++ crates/core/src/toc.rs
@@ -9,14 +9,16 @@ pub struct Toc {
 
 #[derive(Default, Debug, Serialize)]
 pub struct TocSection {
+    num: u32,
     shortcut: String,
     text: String,
     sections: BTreeMap<u32, TocSection>,
 }
 
 impl Toc {
-    fn get_section(&mut self, level: u8) -> &mut TocSection {
+    fn get_section(&mut self, level: u8) -> (u32, &mut TocSection) {
         let len = self.sections.len();
+        let mut num = 0;
 
         if len == 0 || level == 0 {
             self.sections.insert(len as u32, Default::default());
@@ -25,7 +27,9 @@ impl Toc {
         let (_, last) = self.sections.iter_mut().last().unwrap();
 
         if level == 0 {
-            last
+            num = (len as u32) + 1;
+
+            (num, last)
         } else {
             let mut levels_left = level;
 
@@ -38,13 +42,15 @@ impl Toc {
                     last_section.sections.insert(len as u32, Default::default());
                 }
 
+                num = last_section.sections.len() as u32;
+
                 let (_, last_t) = last_section.sections.iter_mut().last().unwrap();
                 last_section = last_t;
 
                 levels_left -= 1;
             }
 
-            last_section
+            (num, last_section)
         }
     }
 
@@ -69,8 +75,9 @@ impl Toc {
             assert!(!self.used_shortcuts.contains(&shortcut));
         }
 
-        let section = self.get_section(level - 1);
+        let (num, section) = self.get_section(level - 1);
 
+        section.num = num;
         section.shortcut = shortcut.clone();
         section.text = title.into();
 
